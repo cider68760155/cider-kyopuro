38e80ba6d9da1c8271012eaff9738ec872097a1c
LAPTOP-4B8SBKO1\asq45 <asq458@outlook.jp>
2019-01-14 23:48:13 +0900

    Update unionFind木.cpp

--- unionFind木.cpp
+++ unionFind木.cpp
@@ -1,31 +1,35 @@
-int N;//—v‘f”
-vi parent;
-vi hight;
+class union_find_tree {
+public:
+	int N;//—v‘f”
+	vi parent;
+	vi hight;
 
-void init() {
-	rep(N) {
-		parent.push_back(i);
-		hight.push_back(0);
+	union_find_tree(int n) {
+		N = n;
+		rep(N) {
+			parent.push_back(i);
+			hight.push_back(0);
+		}
 	}
-}
 
-int find_root(int x) {
-	if (parent[x] == x)return x;
-	else return parent[x] = find_root(parent[x]);
-}
-
-void unite(int x, int y) {
-	x = find_root(x);
-	y = find_root(y);
-	if (x == y)return;
-
-	if (hight[x] <hight[y]) parent[x] = y;
-	else {
-		parent[y] = x;
-		if (hight[x] == hight[y])++hight[x];
+	int find_root(int x) {
+		if (parent[x] == x)return x;
+		else return parent[x] = find_root(parent[x]);
 	}
-}
 
-bool same(int x, int y) {
-	return find_root(x) == find_root(y);
-}
+	void unite(int x, int y) {
+		x = find_root(x);
+		y = find_root(y);
+		if (x == y)return;
+
+		if (hight[x] < hight[y]) parent[x] = y;
+		else {
+			parent[y] = x;
+			if (hight[x] == hight[y])++hight[x];
+		}
+	}
+
+	bool same(int x, int y) {
+		return find_root(x) == find_root(y);
+	}
+};
