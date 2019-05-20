e1ab200564c2013580848ca444dd5a9445727c31
cider68760155 <38486547+cider68760155@users.noreply.github.com>
2019-05-20 11:59:52 +0900

    Update ファイル入出力.cpp

--- ファイル入出力.cpp
+++ ファイル入出力.cpp
@@ -1,5 +1,11 @@
-int main() {
-	ofstream outputfile("memo.txt");
-	outputfile << "‘‚«‚½‚¢‚±‚Æ";
-	outputfile.close();
+#include<fstream>
+
+void file_output(string fileName, string& to_write) {
+	ofstream outputfile(fileName.c_str());
+	outputfile << to_write;
+}
+
+void file_input(string fileName, string& to_read) {
+	ifstream inputfile(fileName.c_str());
+	getline(inputfile, to_read);
 }
